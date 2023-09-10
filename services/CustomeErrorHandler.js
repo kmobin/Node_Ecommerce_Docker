62095b47c848c05ab0cb5124fd90ec9ddd9cb824
kmobin <kmobin434@gmail.com>
2023-09-10 22:11:13 +0530

    login api created

--- services/CustomeErrorHandler.js
+++ services/CustomeErrorHandler.js
@@ -7,6 +7,12 @@ class CustomeErrorHandler extends Error{
     static alreadyExist(message){
         return new CustomeErrorHandler(402, message)
     }
+    static userNotFound(message){
+        return new CustomeErrorHandler(404, message)
+    }
+    static unAuthorized(message){
+        return new CustomeErrorHandler(403,message)
+    }
 }
 
 export default CustomeErrorHandler
